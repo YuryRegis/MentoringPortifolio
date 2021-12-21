da6ef1e3189061300a821e81d0a3f117545ef963
Yury Regis <yury_regis@hotmail.com>
2021-12-21 02:16:13 -0300

    Add meta info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,28 @@ import TimeLine from '../components/TimeLine'
 import Projects from '../components/Projects'
 import { HomeContainer } from '../styles/HomeStyles'
 import Footer from '../components/Footer'
+import Head from 'next/head'
+
 
 
 export function Home() {
   return (
     <HomeContainer>
+      <Head>
+        <title>Home | Meu portfólio</title>
+          <meta
+            name="description"
+            content="Aluno do curso de bacharelado em Ciência da Computação, PUC Minas, unidade de Poços de Caldas."
+          />
+          <meta property="og:image" content="/ogimage.png" />
+          <meta property="og:image:secure_url" content="/ogimage.png" />
+          <meta name="twitter:image" content="/ogimage.png" />
+          <meta name="twitter:image:src" content="/ogimage.png" />
+          <meta
+            property="og:description"
+            content="Aluno do curso de bacharelado em Ciência da Computação, PUC Minas, unidade de Poços de Caldas."
+          />
+      </Head>
       
       <Header />
 
