1644c89a2f5f69a7113b4a379fe652c8c920fc45
Yury Regis <yury_regis@hotmail.com>
2021-12-21 02:12:20 -0300

    Add footer feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,23 @@ import HomeHero from '../components/HomeHero'
 import TimeLine from '../components/TimeLine'
 import Projects from '../components/Projects'
 import { HomeContainer } from '../styles/HomeStyles'
+import Footer from '../components/Footer'
 
 
 export function Home() {
   return (
     <HomeContainer>
+      
       <Header />
+
       <main className='container'>
         <HomeHero />
         <TimeLine />
         <Projects />
       </main>
+
+      <Footer />
+
     </HomeContainer>
   );
 }
