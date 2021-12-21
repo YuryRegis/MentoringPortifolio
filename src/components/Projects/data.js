e0058c449702fdc2e8d603e6f45eb8fa5d2a22e5
Yury Regis <yury_regis@hotmail.com>
2021-12-21 02:47:40 -0300

    Add new projects

--- src/components/Projects/data.js
+++ src/components/Projects/data.js
@@ -1,9 +1,39 @@
 export const data = [
     {
         title: 'Segurança & Criptografia',
-        name: 'Gerador de hash SHA256',
+        name: 'Gerador de hash',
         slug: 'https://github.com/YuryRegis/SEC_ProjetoFinal',
         img: 'https://s10.gifyu.com/images/DevHashTool.gif',
+    },
+    {
+        title: 'Sistemas Operacionais',
+        name: 'Pipe da Velha',
+        slug: 'https://youtu.be/u2ON9YNBaZY',
+        img: 'https://i.ibb.co/r39fwcP/pipe-Da-Velha.png',
+    },
+    {
+        title: 'Modelagem 3D',
+        name: 'Jogo da velha',
+        slug: 'https://github.com/YuryRegis/OpenGl---Jogo-da-Velha',
+        img: 'https://i.ibb.co/1fPSjy5/JOGODAVELHA.png',
+    },
+    {
+        title: 'Modelagem 3D',
+        name: 'Unreal - Projeto Final',
+        slug: 'https://youtu.be/F8f-vWIU9os',
+        img: 'https://i.ibb.co/yqdGHBL/print.png',
+    },
+    {
+        title: 'Prog Dispositivos Móveis',
+        name: 'App Formulário',
+        slug: 'https://github.com/YuryRegis/AppFormulario',
+        img: 'https://i.ibb.co/SnK4Y6f/fomrApp.png',
+    },
+    {
+        title: 'Arquitetura e Estrutura de Dados II',
+        name: 'PenaltySimulator',
+        slug: 'https://github.com/YuryRegis/AEDII_penaltySimulator',
+        img: 'https://user-images.githubusercontent.com/29512626/84350714-37eada80-ab90-11ea-9557-0005745c8fab.png',
     }
 ]
 
